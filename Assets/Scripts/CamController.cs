cecda8828e554fe3c311170a0f22214198577a37
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-05-10 23:21:34 +0800

    ViewController to add view; CamController to rotate

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamController : MonoBehaviour {
+
+    public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ViewController : MonoBehaviour {
+
+    public GameObject viewPrefab;
+
+    private LinkedList<GameObject> viewList = new LinkedList<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.F1)) {
+            createView();
+        }
+	}
+
+    void createView() {
+        GameObject newView = Instantiate(viewPrefab);
+        newView.transform.parent = transform;
+        viewList.AddLast(newView);
+        rearrange();
+    }
+
+    void rearrange() {
+        float firstAngle = - (viewList.Count-1) / 2f * 30;
+        foreach(GameObject view in viewList) {
+            view.transform.eulerAngles = new Vector3(0, firstAngle, 0);
+            firstAngle += 30;
+        }
+    }
+}
