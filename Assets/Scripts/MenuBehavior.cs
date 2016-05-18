d4ad9f04fe75c28bbd60cf612e97fddbdaf64f42
VIVE development area <no-email@cs.nctu.edu.tw>
2016-05-18 18:02:15 +0800

    Use menu key to toggle the menu

--- Assets/Scripts/MenuBehavior.cs
+++ Assets/Scripts/MenuBehavior.cs
@@ -1,28 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuBehavior : MonoBehaviour {
 
 	public static bool isActive {
 		get {
-			return instance.gameObject.activeSelf;
-		}
-		set {
-			instance.gameObject.SetActive (true);
+			return instance.canvas.activeSelf;
 		}
 	}
+	public GameObject camera;
+	public GameObject canvas;
+	public GameObject menuBtn;
 
 	private static MenuBehavior instance;
+
+	private Button createViewBtn;
 
 	// Use this for initialization
 	void Start () {
 		instance = this;
+		createViewBtn = Instantiate (menuBtn).GetComponent<Button>();
+		createViewBtn.transform.SetParent(canvas.transform, false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (InputController.rightController.GetPressDown (SteamVR_Controller.ButtonMask.ApplicationMenu)) {
-			ViewController.instance.createView ();
+			if (isActive) {
+				canvas.SetActive (false);
+			} else {
+				canvas.SetActive (true);
+				transform.eulerAngles = new Vector3(0f, camera.transform.eulerAngles.y, 0f);
+			}
 		}
 	}
 }
