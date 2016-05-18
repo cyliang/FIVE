8d92805081c2d98f7216a4edc7ea8233f1ddd149
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-05-19 03:21:39 +0800

    Use Tracked_Controller & many event driven for InputController

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using System.Collections;
 
 public class InputController: MonoBehaviour {
 	public SteamVR_LaserPointer _laserPointer;
+    public SteamVR_TrackedController controller;
 
 	public static SteamVR_Controller.Device rightController {
 		get {
@@ -18,14 +20,67 @@ public class InputController: MonoBehaviour {
 
 	private static InputController instance;
 
+    private Transform laserOn, clickOn;
+
 	void Start() {
 		instance = this;
 
 		laserPointer.PointerIn += (object sender, PointerEventArgs e) => {
-			EventSystem.current.SetSelectedGameObject (e.target.gameObject);
+            laserOn = e.target;
+            
+            Selectable s = e.target.GetComponent<Selectable>();
+            if (s) {
+                PointerEventData data = new PointerEventData(EventSystem.current);
+                data.pointerPress = null;
+                s.OnPointerEnter(data);
+            }
 		};
 		laserPointer.PointerOut += (object sender, PointerEventArgs e) => {
-			EventSystem.current.SetSelectedGameObject (null);
-		};
-	}
+            laserOn = null;
+
+            Selectable s = e.target.GetComponent<Selectable>();
+            if (s) {
+                PointerEventData data = new PointerEventData(EventSystem.current);
+                data.pointerPress = null;
+                s.OnPointerExit(data);
+            }
+        };
+        controller.TriggerClicked += (object sender, ClickedEventArgs e) => {
+            if (laserOn) {
+                clickOn = laserOn;
+
+                Selectable s = laserOn.GetComponent<Selectable>();
+                if (s) {
+                    PointerEventData data = new PointerEventData(EventSystem.current);
+                    data.pointerPress = laserOn.gameObject;
+                    data.button = PointerEventData.InputButton.Left;
+                    s.OnPointerDown(data);
+                }
+            }
+        };
+        controller.TriggerUnclicked += (object sender, ClickedEventArgs e) => {
+            PointerEventData data = new PointerEventData(EventSystem.current);
+            data.pointerPress = laserOn != null ? laserOn.gameObject : null;
+            data.button = PointerEventData.InputButton.Left;
+
+            if (laserOn != null) {
+                Selectable s = laserOn.GetComponent<Selectable>();
+                if (s)
+                    s.OnPointerUp(data);
+            }
+            if (clickOn != null) {
+                Selectable s = clickOn.GetComponent<Selectable>();
+                if (s)
+                    s.OnPointerUp(data);
+            }
+
+            if (clickOn == laserOn) {
+                Button s = clickOn.GetComponent<Button>();
+                if (s)
+                    s.OnPointerClick(data);
+            }
+        };
+
+        //data.button = PointerEventData.InputButton.Left;
+    }
 }
