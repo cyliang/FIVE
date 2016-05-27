74908a2869a88be156221f45f88c2b1ac63bfb7a
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-05-27 23:11:02 +0800

    List directories

--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -2,6 +2,8 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 public class FileManager: MonoBehaviour {
 
@@ -49,7 +51,8 @@ public class FileManager: MonoBehaviour {
 			});
 		}
     }
-    
+
+
     public string projectPath;
 
     [Header(" [File Browser]")]
@@ -59,23 +62,116 @@ public class FileManager: MonoBehaviour {
     public Button btnConfirm, btnCancel;
     public GameObject itemPrefab, itemSpacePrefab;
 
-	private static FileManager instance;
+    public enum FileBrowserStatus {
+        Closed, ProjectPath, File
+    }
+    private FileBrowserStatus _fileBrowserStatus;
+    public FileBrowserStatus fileBrowserStatus {
+        get {
+            return _fileBrowserStatus;
+        }
+        set {
+            _fileBrowserStatus = value;
+            switch (value) {
+                case FileBrowserStatus.Closed:
+                    FileBrowserCanvas.gameObject.SetActive(false);
+                    break;
+                case FileBrowserStatus.ProjectPath:
+                    FileBrowserCanvas.gameObject.SetActive(true);
+                    initBrowserUntilRoot(projectPathInfo);
+                    drawBrowser(true);
+                    break;
+                case FileBrowserStatus.File:
+                    FileBrowserCanvas.gameObject.SetActive(true);
+                    break;
+            }
+        }
+    }
+
+    private static FileManager instance;
+
+    private DirectoryInfo projectPathInfo;
+
+    private class DirStatus {
+        public int depth;
+        public bool expanded;
+        public DirectoryInfo dirInfo;
+    }
+    private TreeNode<DirStatus> fileBrowserRoot = new TreeNode<DirStatus>(new DirStatus());
 
 	void Start() {
 		instance = this;
+        projectPath = Directory.GetCurrentDirectory();
+        projectPathInfo = new DirectoryInfo(projectPath);
 
         MenuController.addBtn("Change project path", () => {
-            openFileBrowser();
+            fileBrowserStatus = FileBrowserStatus.ProjectPath;
         });
         btnCancel.onClick.AddListener(() => {
-            FileBrowserCanvas.gameObject.SetActive(false);
+            fileBrowserStatus = FileBrowserStatus.Closed;
         });
         btnConfirm.onClick.AddListener(() => {
             /* TODO */
         });
-	}
+        initBrowserUntilRoot(projectPathInfo);
+        drawBrowser(true);
+    }
 
-    void openFileBrowser() {
-        FileBrowserCanvas.gameObject.SetActive(true);
+    void initBrowserUntilRoot(DirectoryInfo dir) {
+        List<DirectoryInfo> parents = new List<DirectoryInfo>();
+        while (dir != null) {
+            parents.Add(dir);
+            dir = dir.Parent;
+        }
+        parents.Reverse();
+
+        TreeNode<DirStatus> d = null;
+        foreach (string drive in Directory.GetLogicalDrives()) {
+            var child = fileBrowserRoot.AddChild(new DirStatus {
+                depth = 0,
+                expanded = false,
+                dirInfo = new DirectoryInfo(drive)
+            });
+            if (drive == parents[0].Name)
+                d = child;
+        }
+        
+        foreach (DirectoryInfo p in parents.Skip(1)) {
+            d = d.AddChild(new DirStatus {
+                depth = d.Value.depth + 1,
+                expanded = false,
+                dirInfo = p
+            });
+        }
+
+        foreach (DirectoryInfo subDir in d.Value.dirInfo.GetDirectories()) {
+            d.AddChild(new DirStatus {
+                depth = d.Value.depth + 1,
+                expanded = false,
+                dirInfo = subDir
+            });
+        }
+    }
+
+    void drawBrowser(bool dirFile) {
+        foreach (var item in fileBrowserRoot.Flatten().Skip(1)) {
+            addBrowserItem(dirFile, item.dirInfo.Name, item.depth);
+        }
+    }
+
+    int addBrowserItem(bool dirFile, string text, int space, int index = -1) {
+        GameObject item = Instantiate(itemPrefab);
+        item.transform.SetParent((dirFile ? directoryContent : fileContent).transform, false);
+        item.GetComponentInChildren<Text>().text = text;
+        if (index != -1)
+            item.transform.SetSiblingIndex(index);
+
+        while (space-- > 0) {
+            GameObject itemSpace = Instantiate(itemSpacePrefab);
+            itemSpace.transform.SetParent(item.transform, false);
+            itemSpace.transform.SetAsFirstSibling();
+        }
+
+        return item.transform.GetSiblingIndex();
     }
 }
---
+++ Assets/Scripts/TreeNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TreeNode<T> {
+    private readonly T _value;
+    private readonly LinkedList<TreeNode<T>> _children = new LinkedList<TreeNode<T>>();
+
+    public TreeNode(T value) {
+        _value = value;
+    }
+
+    public TreeNode<T> Parent { get; private set; }
+
+    public T Value { get { return _value; } }
+
+    public TreeNode<T> AddChild(T value) {
+        var node = new TreeNode<T>(value) { Parent = this };
+        _children.AddLast(node);
+        return node;
+    }
+    
+    public bool RemoveChild(TreeNode<T> node) {
+        return _children.Remove(node);
+    }
+
+    public void Traverse(Action<T> action) {
+        action(Value);
+        foreach (var child in _children)
+            child.Traverse(action);
+    }
+
+    public IEnumerable<T> Flatten() {
+        return new[] { Value }.Union(_children.SelectMany(x => x.Flatten()));
+    }
+}
