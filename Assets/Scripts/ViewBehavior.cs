ce1f4e533266897a59038f846d062a8b90446e5f
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-06-19 03:07:41 +0800

    Use a queue to store querys & will wait web ready
    Correctly load file

--- Assets/Scripts/ViewBehavior.cs
+++ Assets/Scripts/ViewBehavior.cs
@@ -13,9 +13,17 @@ public class ViewBehavior : MonoBehaviour {
     public LinkedListNode<ViewBehavior> selfNode { get; set; }
 	public readonly FileManager.File fileOpened = new FileManager.File();
 
+    class QueryQueueData {
+        public IDictionary qry;
+        public JSEvalDelegate callback;
+    }
+    Queue<QueryQueueData> queryQueue = new Queue<QueryQueueData>();
+    bool queryReady = false;
+
 	// Use this for initialization
 	void Start () {
         isShaking = false;
+        webView.WebQuery += processWebQuery;
 	}
 	
 	// Update is called once per frame
@@ -23,15 +31,30 @@ public class ViewBehavior : MonoBehaviour {
         Vector3 eulerAngle = transform.localEulerAngles;
         eulerAngle.z = isShaking ? Mathf.Sin(Time.time * shakeSpeed) * shakeAmplitude : 0;
         transform.localEulerAngles = eulerAngle;
-
         closeBtn.SetActive(isShaking);
-	}
+
+        if (queryReady && queryQueue.Count > 0) {
+            var qry = queryQueue.Dequeue();
+            webView.EvaluateJavascript(string.Format("makeQuery({0})", UWKJson.Serialize(qry.qry)), qry.callback);
+        }
+    }
 
     public void OnCloseBtnPressed() {
         viewController.removeView(selfNode);
     }
 
     public void makeQuery(IDictionary qry, JSEvalDelegate callback = null) {
-        webView.EvaluateJavascript(string.Format("makeQuery({0})", UWKJson.Serialize(qry)), callback);
+        queryQueue.Enqueue(new QueryQueueData {
+            qry = qry,
+            callback = callback
+        });
+    }
+
+    void processWebQuery(UWKWebQuery webQry) {
+        if (webQry.Request == "ready") {
+            queryReady = true;
+        } else {
+            Debug.Log(webQry.Request);
+        }
     }
 }
