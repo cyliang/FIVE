08fdb7aa9e300624665e3c750a1f9b6ba9aa7b95
VIVE development area <no-email@cs.nctu.edu.tw>
2016-06-19 16:54:32 +0800

    adjust keyboard

---
+++ Assets/KeyBoard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBoard : MonoBehaviour {
+	// Use this for initialization
+	public GameObject Model;
+	void Start () {
+		gameObject.SetActive (false);
+		MenuController.addBtn("Show KeyBoard", () => {
+			if(!gameObject.activeSelf){
+				gameObject.SetActive (true);
+				Model.SetActive (false);
+			}
+			else{
+				gameObject.SetActive (false);
+				Model.SetActive (true);
+			}
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Keyboard/KeyboardButton.cs
+++ Assets/Keyboard/KeyboardButton.cs
@@ -4,29 +4,30 @@ using System.Collections;
 public class NewBehaviourScript : MonoBehaviour {
 
 	public Material red, blue;
-	KeyCode Temp;
+	KeyCode code;
+
 	// Use this for initialization
 	void Start () {
+		foreach (KeyCode vkey in System.Enum.GetValues(typeof(KeyCode))) {
+			if (vkey.ToString () == name) {
+				code = vkey;
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		foreach (KeyCode vkey in System.Enum.GetValues(typeof(KeyCode))) {
-			if (vkey.ToString () == name) {
-				if (Input.GetKeyDown (vkey)) {
-					gameObject.GetComponentInChildren<SpriteRenderer> ().sprite = Resources.Load ("RedSquare", typeof(Sprite)) as Sprite;
-					//gameObject.GetComponent<Renderer> ().material = red;
+		if (Input.GetKeyDown (code)) {
+			gameObject.GetComponentInChildren<SpriteRenderer> ().sprite = Resources.Load ("RedSquare", typeof(Sprite)) as Sprite;
+			//gameObject.GetComponent<Renderer> ().material = red;
 
-					Debug.Log ("Pressed");
-				}
-				if (Input.GetKeyUp (vkey)) {
-					gameObject.GetComponentInChildren<SpriteRenderer> ().sprite = Resources.Load ("BlueSquare", typeof(Sprite)) as Sprite;
-					//gameObject.GetComponent<Renderer> ().material = blue;
+			Debug.Log ("Pressed");
+		}
+		if (Input.GetKeyUp (code)) {
+			gameObject.GetComponentInChildren<SpriteRenderer> ().sprite = Resources.Load ("BlueSquare", typeof(Sprite)) as Sprite;
+			//gameObject.GetComponent<Renderer> ().material = blue;
 
-					Debug.Log ("Released");
-				}
-
-			}
+			Debug.Log ("Released");
 		}
 		/*if(Input.GetKeyDown())
 		{
