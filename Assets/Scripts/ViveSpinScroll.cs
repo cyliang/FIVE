1ee4810c4ad07e04368a31105c552006b21d5fd2
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-06-22 02:30:55 +0800

    Spin event

---
+++ Assets/Scripts/ViveSpinScroll.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveSpinScroll : MonoBehaviour {
+
+    public delegate void SpinScrollEventHandler(int scrollAmount);
+    public event SpinScrollEventHandler SpinScrolled;
+
+    public float degreePerEvent;
+
+    private SteamVR_TrackedController controller;
+    private bool previousTouched;
+    private float previousAngle;
+    private float accumulatedSpin;
+
+	void Start () {
+        controller = GetComponent<SteamVR_TrackedController>();
+        previousTouched = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!controller.padTouched) {
+            previousTouched = false;
+            return;
+        }
+
+        float nowAngle = calculateAngle(controller.controllerState.rAxis0);
+        if (!previousTouched) {
+            previousTouched = true;
+            accumulatedSpin = 0;
+
+        } else {
+            accumulatedSpin += Mathf.DeltaAngle(previousAngle, nowAngle);
+            int scrollAmount = (int)(accumulatedSpin / degreePerEvent);
+            if (Mathf.Abs(scrollAmount) >= 1) {
+                accumulatedSpin -= scrollAmount * degreePerEvent;
+                SpinScrolled(-scrollAmount);
+            }
+        }
+        previousAngle = nowAngle;
+    }
+
+    float calculateAngle(Valve.VR.VRControllerAxis_t coor) {
+        return Mathf.Atan2(coor.y, coor.x) * Mathf.Rad2Deg;
+    }
+}
