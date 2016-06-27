868dba07c75d0926b74b8c14551e3e7bd90a45b3
VIVE development area <no-email@cs.nctu.edu.tw>
2016-06-28 02:35:01 +0800

    Fix button clicking not react

--- Assets/Keyboard/KeyboardButton.cs
+++ Assets/Keyboard/KeyboardButton.cs
@@ -19,30 +19,10 @@ public class KeyboardButton : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown (code)) {
 			gameObject.GetComponentInChildren<SpriteRenderer> ().sprite = Resources.Load ("RedSquare", typeof(Sprite)) as Sprite;
-			//gameObject.GetComponent<Renderer> ().material = red;
-
-			Debug.Log ("Pressed");
 		}
 		if (Input.GetKeyUp (code)) {
 			gameObject.GetComponentInChildren<SpriteRenderer> ().sprite = Resources.Load ("BlueSquare", typeof(Sprite)) as Sprite;
-			//gameObject.GetComponent<Renderer> ().material = blue;
-
-			Debug.Log ("Released");
 		}
-		/*if(Input.GetKeyDown())
-		{
-			gameObject.GetComponentInChildren<SpriteRenderer>().sprite = Resources.Load("RedSquare", typeof(Sprite)) as Sprite;
-			gameObject.GetComponent<Renderer> ().material = red;
-
-			Debug.Log("Pressed");
-		}
-		if(Input.GetKeyUp(KeyCode.A))
-		{
-			gameObject.GetComponentInChildren<SpriteRenderer>().sprite = Resources.Load("BlueSquare", typeof(Sprite)) as Sprite;
-			gameObject.GetComponent<Renderer> ().material = blue;
-
-			Debug.Log("Released");
-		}*/
 	
 	}
 	
